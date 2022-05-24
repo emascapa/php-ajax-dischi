f7c566bf2ba2812b7b03890adc7c2ac258e8315a
emascapa <emascapa@gmail.com>
2022-05-24 17:35:43 +0200

    Add milestone 2

---
+++ milestone-2/src/components/SpotifyComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <SpotifyHeader />
+    <SpotifyMain />
+  </div>
+</template>
+
+<script>
+import SpotifyHeader from "@/components/SpotifyHeader.vue";
+import SpotifyMain from "@/components/SpotifyMain.vue";
+
+export default {
+  name: "SpotifyComponent",
+  components: {
+    SpotifyHeader,
+    SpotifyMain,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ milestone-2/src/components/SpotifyMain.vue
@@ -0,0 +1,126 @@
+<template>
+  <main>
+    <div class="container py-4">
+      <div
+        v-if="!loading && filteredSpotifyItems.length > 0"
+        class="row row-cols-2 row-cols-sm-3 row-cols-md-5 g-3"
+      >
+        <!-- <div class="col" v-for="(item, index) in albumList" :key="index"> -->
+        <div
+          class="col"
+          v-for="(item, index) in filteredSpotifyItems"
+          :key="index"
+        >
+          <SpotifyCard
+            :cover="item.poster"
+            :albumTitle="item.title"
+            :band="item.author"
+            :year="item.year"
+          />
+        </div>
+      </div>
+      <div
+        v-else-if="!loading && filteredSpotifyItems.length == 0"
+        class="else_div d-flex justify-content-center align-items-center"
+      >
+        <span>No result match</span>
+      </div>
+      <div
+        v-else
+        class="else_div d-flex justify-content-center align-items-center"
+      >
+        <span>Loading ...</span>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import state from "@/state.js";
+import SpotifyCard from "@/components/SpotifyCard.vue";
+
+import axios from "axios";
+
+export default {
+  name: "SpotifyMain",
+  components: {
+    SpotifyCard,
+  },
+  data() {
+    return {
+      //questo era il vecchio 
+      //spotifyLink: "https://flynn.boolean.careers/exercises/api/array/music",
+
+      //questo è la nuova api al db mamp
+      spotifyLink: "http://localhost/php-ajax-dischi/milestone-2/public/db.php",
+      albumList: null,
+      errorMessage: null,
+      loading: true,
+    };
+  },
+  mounted() {
+    axios
+      .get(this.spotifyLink)
+      .then((response) => {
+        //console.log('CIAOOOO');
+
+        this.albumList = response.data;
+        this.loading = false;
+
+        console.log("Spotify ok");
+      })
+      .catch((error) => {
+        this.errorMessage = `Sorry There is a problem! ${error}`;
+      });
+  },
+  computed: {
+    filteredSpotifyItems() {
+      //cancella
+      console.log(`array spotify:`);
+      console.log(this.albumList);
+
+      console.log(`primo elemento dell'array spotify:`);
+      console.log(this.albumList[0]);
+
+      console.log(`chiave del primo elemento dell'array spotify:`);
+      console.log(this.albumList[0].title);
+
+      return this.albumList.filter((item) => {
+        if (state.observableSpotifySelect == "1") {
+          return item.title
+            .toLowerCase()
+            .includes(state.observableSpotifySearch.toLowerCase());
+        } else if (state.observableSpotifySelect == "2") {
+          return item.author
+            .toLowerCase()
+            .includes(state.observableSpotifySearch.toLowerCase());
+        } else if (state.observableSpotifySelect == "3") {
+          return item.year
+            .toLowerCase()
+            .includes(state.observableSpotifySearch.toLowerCase());
+        } else {
+          return item.genre
+            .toLowerCase()
+            .includes(state.observableSpotifySearch.toLowerCase());
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  background-color: $bg_theme;
+  
+  min-height: calc(100vh - 60px);
+
+  /*overflow-y: scroll; */
+
+  .else_div {
+    color: $light_theme;
+    height: calc((100vh - 120px) / 2);
+  }
+}
+</style>
+
